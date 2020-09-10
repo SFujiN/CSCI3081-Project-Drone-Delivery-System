44adf7ee0d5739da9cd1291b75cb9a8d8cfb43e9
Carter Blum <blumx116@umn.edu>
2020-09-10 10:58:47 -0500

    initial commit - added labs

---
+++ bin/build-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ROOTDIR=`git rev-parse --show-toplevel`
+docker build $1 \
+	--target env \
+	--build-arg USER_ID=$(id -u) \
+	--build-arg GROUP_ID=$(id -g) \
+	-t csci3081f20/env "$ROOTDIR"
+mkdir -p .dependencies
+docker run -v "${ROOTDIR}:/home/user/repo" csci3081f20/env cp -r ../../../project/grades/Fall-2020/csci3081/dependencies/include .dependencies
+
