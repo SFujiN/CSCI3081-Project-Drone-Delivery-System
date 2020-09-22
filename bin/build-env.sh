a9b2c1e5740cfc2a456a1d973c515ec52f5f3cc2
dtorban <dtorban@umn.edu>
2020-09-22 12:38:19 -0500

    Update build-env.sh

--- bin/build-env.sh
+++ bin/build-env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker pull csci3081f20/base
+
+CONTAINER=csci3081f20-env-container
 ROOTDIR=`git rev-parse --show-toplevel`
 docker build $1 \
 	--target env \
@@ -7,5 +10,6 @@ docker build $1 \
 	--build-arg GROUP_ID=$(id -g) \
 	-t csci3081f20/env "$ROOTDIR"
 mkdir -p .dependencies
-docker run -v "${ROOTDIR}:/home/user/repo" csci3081f20/env cp -r ../../../project/grades/Fall-2020/csci3081/dependencies/include .dependencies
+docker rm ${CONTAINER}
+docker run --name=${CONTAINER} -v "${ROOTDIR}:/home/user/repo" csci3081f20/env cp -r ../../../project/grades/Fall-2020/csci3081/dependencies/include .dependencies
 
