846ef944969d710f41d9e141a961340e537e8e9e
Allison Beckstrom <fitch108@umn.edu>
2020-11-12 15:13:11 -0600

    Port routing scheduler, main functionality is TODO

---
+++ project/src/routing_scheduler.cc
@@ -0,0 +1,30 @@
+#include "src/routing_scheduler.h"
+#include "src/route_utils.h"
+#include <vector>
+#include <limits>
+
+namespace csci3081 {
+
+void csci3081::RoutingScheduler::ScheduleDelivery(
+        csci3081::Package* package, csci3081::Customer* customer,
+        const std::vector<entity_project::Entity*>& entities
+) {
+  package->NotifyScheduled();
+  Drone* to_dispatch = findAppropriateDrone(
+          package, customer, entities);
+  if (to_dispatch == nullptr) {
+    std::cerr << "No drone was a available for this delivery! Try again later!" << std::endl;
+    return;
+  }
+  // TODO: Tell the drone where to go
+  //to_dispatch->SetDeliveryPlan(package, customer);
+}
+
+Drone* RoutingScheduler::findAppropriateDrone(
+        Package* package, Customer* customer,
+        const std::vector<entity_project::Entity*>& vector) {
+  // TODO
+  return nullptr;
+}
+
+}  // namespace csci3081
