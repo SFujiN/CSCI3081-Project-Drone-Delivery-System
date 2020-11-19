017e96c34ce45b3375d91e5fc91e21d11a79e283
Mason Dale <dale0176@umn.edu>
2020-11-19 09:34:07 -0600

    docs(package.h): Added documentation to some functions and variables.

--- project/src/package.h
+++ project/src/package.h
@@ -8,13 +8,13 @@
 
 namespace csci3081 {
 
-/// TODO: Add documentation. You may edit this class including adding members or methods.
+/// Represents a package in the drone system.
 /**
- * TODO: Add detailed documentation.
+ * @brief Package class
  */
 class Package : public entity_project::Package {
  public:
-  /// TODO: Add documentation.
+  /// @brief Default package constructor
   Package() { AddType<Package>(); }
   /**
    * @brief Create a Package from the JSON encoding.
@@ -23,20 +23,38 @@ class Package : public entity_project::Package {
   /// Provides the package weight
   float GetWeight() const override;
 
-  /// Tell the drone that it has been picked up so it becomes dynamic
+  /**
+  * Tell the drone that it has been picked up so it becomes dynamic.
+  * Call's Notify from Observable& to notify observers that package is
+  * scheduled.
+  */
   void NotifyScheduled();
+  /**
+  * Called when package has been delivered, and sets HasBeenDelivered
+  * to true. Call's Notify from Observable& to notify observers that package is
+  * delivered.
+  */
   void NotifyDelivered();
+  /**
+  * Called when package has been picked up. Call's Notify from Observable&
+  * to notify observers that package is picked up and en route.
+  */
   void NotifyPickedUp();
-
+  /**
+  * Retrieves a Package's Observable reference to call functions
+  * related to the subject of an observer pattern. 
+  * 
+  * @return Observable object reference. Returns packageObservable
+  */
   Observable& GetObservable() { return packageObservable; }
 
   bool ShouldDelete() { return HasBeenDelivered; }
   bool IsDynamic() const override { return HasBeenScheduled; }
 
  private:
-  bool HasBeenScheduled = false;
-  bool HasBeenDelivered = false;
-  Observable packageObservable;
+  bool HasBeenScheduled = false;  ///< boolean to track scheduled state
+  bool HasBeenDelivered = false;  ///< boolean to track delivered state
+  Observable packageObservable;  ///< Calls subject functionality
 };
 
 }  // namespace csci3081
