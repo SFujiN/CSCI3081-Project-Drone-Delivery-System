1e433b31b533392446562fd89ccb5c0e9533d1e4
Mason Dale <dale0176@umn.edu>
2020-11-18 20:13:02 -0600

    refactor(observable.cc): Remove dead code in Notify(..)
    -Removed dead code in Notify(..) that's not needed for OnEvent(..)

--- project/src/observable.cc
+++ project/src/observable.cc
@@ -6,7 +6,6 @@ namespace csci3081 {
 
 void Observable::Notify(const picojson::value& event) {
     for (std::vector<entity_project::EntityObserver*>::const_iterator iter = observers.begin(); iter != observers.end(); ++iter) {
-        const entity_project::Entity& ent = *entity;
         (*iter)->OnEvent(event, *entity);
     }
 }
