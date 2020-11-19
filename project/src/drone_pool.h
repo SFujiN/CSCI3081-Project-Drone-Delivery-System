87e197d5a37bcea47743ade09b76315279c77f24
Donald <huynh338@umn.edu>
2020-11-19 16:28:15 -0600

    adding default values to the droneSpecs constructor

--- project/src/drone_pool.h
+++ project/src/drone_pool.h
@@ -18,11 +18,12 @@ namespace csci3081 {
 	float weight_cap_;
 	int base_bat_cap_;
 
-	droneSpecs(std::string model,
-               float mass,
-               float speed,
-               float acc, float weight,
-               int battery)
+	droneSpecs(std::string model = "Q-36-01",
+               float mass = 50.0,
+               float speed = 60.0,
+               float acc = 4.0,
+               float weight = 10.0,
+               int battery = 300)
                : model_(model),
                  mass_(mass),
                  max_speed_(speed),
