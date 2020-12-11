337557213b5278380e7bd1b7ace7a8c11cbf922b
huynh338 <huynh338@umn.edu>
2020-12-11 08:27:01 -0600

    statistics test

---
+++ project/tests/statistics_integration.cc
@@ -0,0 +1,75 @@
+#include <EntityProject/osm_graph_parser.h>
+#include <EntityProject/project_settings.h>
+
+#include <iostream>
+
+#include "EntityProject/ANVIL/drone_delivery_system.h"
+#include "gtest/gtest.h"
+#include "src/customer.h"
+#include "src/drone.h"
+#include "src/json_helper.h"
+#include "src/package.h"
+#include "src/statistics.h"
+
+namespace csci3081 {
+
+using entity_project::DroneDeliverySystem;
+using entity_project::Entity;
+
+class TestStatistics : public csci3081::Statistics {
+ public:
+  //   std::unordered_map<int, PackData>GetPackageData() { return package_data;
+  //   } std::unordered_map<int, DroneData>GetDroneData() { return drone_data; }
+  //   float GetSimTime() { return simulation_time; }
+  //   FRIENDTEST(StatisticsIntegration, PackageTest);
+  //   FRIENDTEST(StatisticsIntegration, DroneTest);
+};
+
+class StatisticsIntegration : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    system = GetDroneDeliverySystem("default");
+    entity_project::OSMGraphParser parser;
+    const entity_project::IGraph* graph =
+        parser.CreateGraph("data/umn.osm", "data/umn-height.csv");
+    system->SetGraph(graph);
+
+    p_obj["type"] = picojson::value("package");
+    p_obj["name"] = picojson::value("TEST PACKAGE 1");
+    p_obj["position"] = picojson::value(picojson::array(
+        {picojson::value(1.0), picojson::value(2.0), picojson::value(3.0)}));
+    package = system->CreateEntity(p_obj)->AsType<Package>();
+    system->AddEntity(package);
+
+    d_obj["type"] = picojson::value("drone");
+    d_obj["name"] = picojson::value("TEST DRONE 1");
+    d_obj["position"] = picojson::value(picojson::array(
+        {picojson::value(0.0), picojson::value(0.0), picojson::value(0.0)}));
+    drone = system->CreateEntity(d_obj)->AsType<Drone>();
+    system->AddEntity(drone);
+  }
+
+  virtual void TearDown() { delete system; }
+
+  DroneDeliverySystem* system;
+  picojson::object p_obj;
+  picojson::object d_obj;
+  Package* package;
+  Drone* drone;
+};
+
+TEST_F(StatisticsIntegration, SimulationTime) {
+  EXPECT_FLOAT_EQ(Statistics::GetInstance()->GetSimTime(), 0);
+  Statistics::GetInstance()->Update(30);
+  EXPECT_NE(Statistics::GetInstance()->GetSimTime(), 0);
+}
+
+TEST_F(StatisticsIntegration, PackageTest) {
+  system->AddObserver(package, Statistics::GetInstance());
+}
+
+TEST_F(StatisticsIntegration, DroneTest) {
+  system->AddObserver(drone, Statistics::GetInstance());
+}
+
+}  // namespace csci3081
